69defa4b41c1f8371ebd5951907bf76ca997737d
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-01 16:49:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,3 +5,10 @@ def find_element_index(array, value_to_find)
     count += 1
   end
 end
+def find_element_index(array, value_to_find)
+  count = 0
+  while count < array.length do
+    puts array[count] == value_to_find
+    count += 1
+  end
+end
